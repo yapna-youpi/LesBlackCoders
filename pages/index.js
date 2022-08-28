8e6538103b997ebfba7c77a9b11274d568bf7fd7
yapna-youpi <yapnayoupi@gmail.com>
2022-08-28 04:27:18 +0100

    initialisation du projet le 28/08/2022

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>BlackCoders</title>
+        <meta name="description" content="site for enterprise Blackcoders" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+
+      </main>
+        
+    </div>
+  )
+}
